bcae41ceb612b5c26d43a77ecde1a47436da5da0
ankit12332 <ankit.panda010.ap@gmail.com>
2023-08-20 13:16:07 +0530

    created a code for connection to database with dotnet core api

---
+++ conectionStringDemoPostgresql/Data/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using conectionStringDemoPostgresql.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace conectionStringDemoPostgresql.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ conectionStringDemoPostgresql/Model/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace conectionStringDemoPostgresql.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
